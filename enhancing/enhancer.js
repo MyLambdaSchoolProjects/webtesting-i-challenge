dd4159f19307a3b1b88bd73245b24978a5e42d4d
David Isakson <ikeman32@yahoo.com>
2020-03-25 20:21:18 -0700

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,37 +1,91 @@
-const isObject = require('isobject');
+const isObject = require("isobject");
 
 module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if (isObject(item)) {
+    //console.log('length',Object.keys(item).length);
+    if (Object.keys(item).length !== 3) {
+      throw new Error();
+      // }else{
+      //   item.durability < 100 ? item.durability = 100: {...item};
+    }
+    if (typeof item.name !== "string") {
+      throw new Error();
+    }
+    if (
+      !Number.isInteger(item.durability) ||
+      !Number.isInteger(item.enhancement)
+    ) {
+      throw new Error();
+    }
+    item.enhancement < 20 ? item.enhancement++ : { ...item };
+  } else {
+    throw new Error();
+  }
+
+  //return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+  if (isObject(item)) {
+    //console.log('length',Object.keys(item).length);
+    if (Object.keys(item).length !== 3) {
+      throw new Error();
+      // }else{
+      //   item.durability < 100 ? item.durability = 100: {...item};
+    }
+    if (typeof item.name !== "string") {
+      throw new Error();
+    }
+    if (
+      !Number.isInteger(item.durability) ||
+      !Number.isInteger(item.enhancement)
+    ) {
+      throw new Error();
+    }
+    if (item.enhancement < 15) {
+      item.durability -= 5;
+    } else if (item.enhancement === 15) {
+      item.durability -= 10;
+    } else if (item.enhancement >= 16) {
+      item.durability -= 10;
+      item.enhancement -= 1;
+    } else {
+      return { ...item };
+    }
+  } else {
+    throw new Error();
+  }
 }
 
 function repair(item) {
-  if(isObject(item)){
+  if (isObject(item)) {
     //console.log('length',Object.keys(item).length);
-    if(Object.keys(item).length !== 3){
-        throw new Error;
+    if (Object.keys(item).length !== 3) {
+      throw new Error();
       // }else{
       //   item.durability < 100 ? item.durability = 100: {...item};
-      }
-    if(typeof item.name !== 'string'){throw new Error;}
-    if(!Number.isInteger(item.durability) || !Number.isInteger(item.enhancement)){
-      throw new Error;
     }
-      item.durability < 100 ? item.durability = 100: {...item};
-  }else{
-    throw new Error;
+    if (typeof item.name !== "string") {
+      throw new Error();
+    }
+    if (
+      !Number.isInteger(item.durability) ||
+      !Number.isInteger(item.enhancement)
+    ) {
+      throw new Error();
+    }
+    item.durability < 100 ? (item.durability = 100) : { ...item };
+  } else {
+    throw new Error();
   }
-  
+
   //return { ...item };
 }
 
